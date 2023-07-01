36b9b4efd55fe7a4f182d8cf3f57a594b6087a68
volker <volker.jaenisch@inqbus.de>
2023-07-01 23:33:28 +0200

    added timestamp for log

--- MicroPython/somose_main.py
+++ MicroPython/somose_main.py
@@ -1,22 +1,35 @@
-from machine import Pin, I2C, Timer
+from machine import Pin, I2C, Timer, RTC
 
 from somose import SoMoSe
 
+rtc = RTC()
+rtc.datetime((2023, 7, 1, 0, 0, 0, 0, 0))
+
+def log(msg):
+    now = rtc.datetime()
+    now_str = f'{now[0]:.4d}-{now[1]:02d}-{now[2]:02d} {now[4]:02d}:{now[5]:02d}:{now[6]:02d}' 
+    print(now_str, msg)
+
+
 i2c = I2C(0, sda=Pin(18), scl=Pin(19))
 
-print("I2C Scan: should show '[85]'")
+log("I2C Scan: should show '[85]'")
 
 scan_res = i2c.scan()
 
-print(f'scan result: {scan_res}')
+log(f'scan result: {scan_res}')
 
 somose_sensor = SoMoSe(i2c)
+
+log('Waiting 10 seconds for first timer call')
+
 
 
 def get_moisture(_timer):
     
+    log('Doing measurement')
     mean, current = somose_sensor.measure()
-    print(f'mean: {mean}, current: {current}')
+    log(f'mean: {mean}, current: {current}')
     
     
 timer0 = Timer(0)
@@ -25,4 +38,5 @@ timer0.init(period=10000, mode=Timer.PERIODIC, callback=get_moisture)
 
 
 
+               
                
